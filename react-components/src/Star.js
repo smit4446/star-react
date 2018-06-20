e61e7fbd40a2c245c38aebb9d58cccb0826447ea
smit4446 <smit4446@gmail.com>
2018-06-20 15:09:51 -0500

    add form capabilities

--- react-components/src/Star.js
+++ react-components/src/Star.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 class Star extends Component {
     constructor(props) {
         super(props);
-        this.state = { star: {name: '', role: ''}};
-        this.handleClick = this.handleClick.bind(this);
+        this.state = { star: {name: '', role: ''}, starList: []};
     }
 
     handleChangeFor = (propertyName) => (event) => {
@@ -16,18 +15,32 @@ class Star extends Component {
         });
     }
 
-    handleClick () {
-        console.log(this.state.star.name,'is famous for', this.state.star.role);
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({starList: [...this.state.starList, this.state.star]})
+        this.setState({star: {name: '', role: ''}});
+        console.log(this.state.star);
     }
+
+    // handleClick () {
+    //     console.log(this.state.star.name,'is famous for', this.state.star.role);
+    // }
     
     render() {
         return (
             <div>
             <h2>The Best Star Ever</h2>
-            <input onChange={this.handleChangeFor('name')} />
-            <input onChange={this.handleChangeFor('role')} />
+            <form onSubmit={this.handleSubmit}>
+            <p>Name: <input onChange={this.handleChangeFor('name')} value={this.state.star.name} placeholder='Name'/></p>
+            <p>Role: <input onChange={this.handleChangeFor('role')} value={this.state.star.role} placeholder='Role'/></p>
             <button onClick={this.handleClick}>LOG THE OBJECT</button>
+            </form>
             <p>{this.state.star.name} is famous for {this.state.star.role}</p>
+            <ul>
+                {this.state.starList.map(star =>
+                <li key={star.name}>{star.name} is famous for {star.role}</li>
+                )}
+            </ul>
             </div>  
         )
     }
